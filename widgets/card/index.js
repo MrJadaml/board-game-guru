6bfdbe8cda30986ef1c0ef7262a2f29d77aa4bd9
mrjadaml <mrjadaml@gmail.com>
2019-11-25 17:41:58 -0800

    sass modules

--- widgets/card/index.js
+++ widgets/card/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
+import styles from './card.scss'
 
-export default function Card({ title, id, slug, ...rest}) {
+export default function Card({ title, id, slug, ...rest }) {
   return (
-    <Link href={`/game/${slug}`} key={slug}>
-      <h3>{title}</h3>
+    <Link href={`/game/${slug}`} key={slug} className="card">
+      <h3 className={styles.card}>{title}</h3>
     </Link>
   )
 }
