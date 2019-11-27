7b042a19dbd48f709414e474a8ccfaa6210ccbe2
mrjadaml <mrjadaml@gmail.com>
2019-11-27 07:51:31 -0800

    linking 🔗

--- widgets/card/index.js
+++ widgets/card/index.js
@@ -2,12 +2,11 @@ import React from 'react'
 import Link from 'next/link'
 import styles from './card.scss'
 
-export default function Card({ title, idx, slug, ...rest }) {
-
+export default function Card({ title, idx, href, ...rest }) {
   const bgColorClass = `color-${idx % 3}`
 
   return (
-    <Link href={`/game/${slug}`} key={slug}>
+    <Link href={href} key={idx}>
       <div className={`${styles.card} ${styles[bgColorClass]}`}>
         <h3>{title}</h3>
       </div>
--- widgets/list/index.js
+++ widgets/list/index.js
@@ -3,10 +3,14 @@ import { map } from 'lodash'
 import typeMap from '../component-type-map';
 import PropTypes from 'prop-types';
 
+let idx = 0;
+
 export const List = ({ collection, componentType }) => {
   return (
     <div>
-      {map(collection, (itemProps, idx) => {
+      {map(collection, itemProps => {
+        idx = idx + 1
+
         return React.createElement(
           typeMap[componentType],
           { idx, ...itemProps },
@@ -17,12 +21,12 @@ export const List = ({ collection, componentType }) => {
   )
 }
 
-List.propTypes = {
-  collection: PropTypes.array,
-  componentType: PropTypes.string,
-};
+// List.propTypes = {
+  // collection: PropTypes.oneOf([PropTypes.array, PropTypes.object]),
+  // componentType: PropTypes.string,
+// };
 
-List.defaultProps = {
-  collection: [],
-  componentType: 'item',
-};
+// List.defaultProps = {
+  // collection: [],
+  // componentType: 'item',
+// };
